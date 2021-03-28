edbb9dd2486479a3810c4375423133fd61e51b2e
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-03-28 23:19:35 +0530

    creating sqllite database

--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:maps/models/place_model.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -19,5 +20,11 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
+    //inserting data as map
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
